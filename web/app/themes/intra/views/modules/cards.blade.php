fb0164983ff0eef0685ecda2dbaec6971011a818
Andreas Ek <andreas@andreasek.se>
2016-01-07 20:19:40 +0100

    Cards, image source

--- web/app/themes/intra/views/modules/cards.blade.php
+++ web/app/themes/intra/views/modules/cards.blade.php
@@ -9,7 +9,8 @@
 	<div class="panel-body">
 		@if( has_post_thumbnail( $module->ID ) )
 			<div class="thumbnail">
-			    <?=get_the_post_thumbnail( $module->ID, ['class'=>'img-rounded','size'=>'full']) ?>
+			    <?php $feat_image_url = wp_get_attachment_url( get_post_thumbnail_id( $module->ID ) ); ?>
+				<img src="{{ $feat_image_url }}" class="img-responsive" />
 			</div>
 		@endif
 		<p>{!! papi_get_field( $module->ID, 'text' ) !!}</p>
