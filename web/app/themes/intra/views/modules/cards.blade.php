5aef50cfdf4f0545c1679a4dd636af85447682ff
Andreas Ek <andreas@andreasek.se>
2016-01-07 20:05:04 +0100

    Cards fixed

--- web/app/themes/intra/views/modules/cards.blade.php
+++ web/app/themes/intra/views/modules/cards.blade.php
@@ -1,17 +1,27 @@
 <?php 
 	$link = papi_get_field( $module->ID, 'link') 
 ?>
-<div class="thumbnail">
-	@if( has_post_thumbnail( $module->ID ) )
-		    {!! get_the_post_thumbnail( $module->ID, ['class'=>'img-rounded']) !!}
-	@endif
-	<div class="caption text-center">
-		<h3>{!! get_the_title( $module->ID ) !!}</h3>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">{{ get_the_title( $module->ID ) }}</h3>
+	</div>
+	<div class="panel-body">
+		@if( has_post_thumbnail( $module->ID ) )
+			<div class="thumbnail">
+			    {!! get_the_post_thumbnail( $module->ID, ['class'=>'img-rounded','size'=>'medium']) !!}
+			</div>
+		@endif
 		<p>{!! papi_get_field( $module->ID, 'text' ) !!}</p>
 		@if( $link )
-			<p>
-				<a href="{{ $link->url }}" class="btn btn-warning">{{ $link->title }}</a>
-			</p>
+			<div class="caption text-center">
+					<p>
+						<a href="{{ $link->url }}" class="btn btn-warning">{{ $link->title }}</a>
+					</p>
+			</div>
 		@endif
 	</div>
 </div>
+
+
+
